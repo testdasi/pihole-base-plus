3f56c6389245195babf93d61b00a850d4321c765
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-08 12:12:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,29 +20,32 @@ if [[ ${TARGETPLATFORM} =~ "arm64" ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture arm64
+    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
 elif [[ ${TARGETPLATFORM} =~ "amd64" ]]
 then 
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture amd64
+    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
 elif [[ ${TARGETPLATFORM} =~ "386" ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture 386
+    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
 elif [[ ${TARGETPLATFORM} =~ 'arm/v7' ]]
 then
     curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture arm
+    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
 #elif [[ ${TARGETPLATFORM} =~ 'arm/v6' ]]
 #then
 #    curl -sL https://hobin.ca/cloudflared/releases/2022.3.1/cloudflared_2022.3.1_arm.deb -o /tmp/cloudflared.deb
 else 
-    echo "$(date "+%d.%m.%Y %T") CRITICAL - Invalid platform build" >> /build.info
+    echo "$(date "+%d.%m.%Y %T") Unsupported platform - cloudflared not added" >> /build.info
 fi
 apt install /tmp/cloudflared.deb \
     && rm -f /tmp/cloudflared.deb \
     && useradd -s /usr/sbin/nologin -r -M cloudflared \
-    && chown cloudflared:cloudflared /usr/local/bin/cloudflared \
-    && echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
+    && chown cloudflared:cloudflared /usr/local/bin/cloudflared
 
 # clean cloudflared config
 mkdir -p /etc/cloudflared \
